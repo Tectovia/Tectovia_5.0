d74570f547d1df66b5ceb6bc9e1022f08dd6310e
Tectovia <tectovia@gmail.com>
2024-03-24 13:59:33 +0530

    Your commit message here

--- server/database/database.js
+++ server/database/database.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.LOCALDBURL, {
+mongoose.connect(process.env.ATDBURL, {
 
   useNewUrlParser: true,
   useUnifiedTopology: true,
