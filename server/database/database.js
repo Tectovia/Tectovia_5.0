71e22e3cbc36469710e58443fc698711881b3dfd
Tectovia <tectovia@gmail.com>
2024-03-24 17:19:06 +0530

    commiting changes and merged on 5:19 24/03

--- server/database/database.js
+++ server/database/database.js
@@ -1,14 +1,13 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.ATDBURL, {
+mongoose.connect(process.env.LOCALDBURL, {
 
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
 const db = mongoose.connection;
-
 
 mongoose.set("strictQuery", false);
 db.on("error", console.error.bind(console, "connection error:"));-``  
